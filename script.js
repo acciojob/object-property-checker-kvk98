ffb48331bde83390c5460fc3fa479545f1b077f4
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-11 12:59:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	for (let i in sampleObject) {
-		if (i==key) {
-			return true;
-		}
-	} 
-	return false;
+	let answer = sampleObject.hasOwnProperty(key);
+	return answer;
+	// for (let i in sampleObject) {
+	// 	if (i==key) {
+	// 		return true;
+	// 	}
+	// } 
+	// return false;
 	
 }
 
